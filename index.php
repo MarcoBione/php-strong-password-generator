6f8b8fd05a779974d692f0636b86ed51a2806a18
MarcoBione <marcobione.work@gmail.com>
2023-05-05 16:48:14 +0200

    Add all in index.php + remove PHP folder

--- index.php
+++ index.php
@@ -14,7 +14,40 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
 Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
 */
-
-
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP|Secure PSW gen</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+    <h1>Crea la tua password</h1>
+    <p>in modo semplice e veloce!</p>
+</header>
+
+<main>
+    <div>
+
+        <form class="row g-3" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+
+        
+
+            <button type="submit" class="btn btn-outline-success">Genera</button>
+
+
+        </form>
+
+    </div>
+
+</main>
+    
+</body>
+</html>
