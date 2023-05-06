bc1c4778d5f718026814eba1f4bddac66907c1d9
MarcoBione <marcobione.work@gmail.com>
2023-05-06 11:27:43 +0200

    Add some style

--- index.php
+++ index.php
@@ -14,67 +14,83 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
 Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
 */
-
+$psw = 'la tua password sarà qui';
 
 if(isset($_GET['pswnum'])){
-    $numOfCaracthers=$_GET['pswnum'];
-    
-    $psw = genPassword($numOfCaracthers);
-    //chiamo la funzione qua e gli passo il numero!
-    echo $psw;
-    
+
+    $max = 32;
+    $min = 8;
+
+    //check if number is alterated
+    if($_GET['pswnum'] <= $max && $_GET['pswnum'] >= $min ){
+       
+        $numOfCaracthers=$_GET['pswnum'];
+        $psw = genPassword($numOfCaracthers);
+    }else{
+        $psw='qualcosa è andato storto';
+    }
 }
+
+//PSW Generator
 function genPassword ($numOfCaracthers){
     //echo $numOfCaracthers;
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#!"$%&/=^'; //character aviable
-    $password = substr(str_shuffle($chars), 0, $numOfCaracthers);
-    return $password;
+    $password = substr(str_shuffle($chars), 0, $numOfCaracthers); //sorting n° characters from string randomly
+    return $password; //return password from function call
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP|Secure PSW gen</title>
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <!-- my CSS -->
-    <link rel="stylesheet" href="./CSS/master.css">
-</head>
+<!-- base html template -->
+<?php 
+include __DIR__ . '/template.php';
+?>
+<!-- end base html template -->
+
 <body>
 
-<header>
-    <div class="container text-center">
-       <h1>Crea la tua password</h1>
-        <p>in modo semplice e veloce!</p> 
+    <div class="_mywrapper">
+        <div class="_myblur">
+
+            <header class="p-4">
+                <div>
+                   <h2 class="_mytxt">PSW.</h2>
+                    <h2 class="_mytxt">GEN.rator  -V1</h2>
+                </div>
+            </header>
+
+            <main class="d-flex flex-column justify-content-start align-items-center p-4">
+
+                <div class="_mycontainer mb-5">
+
+                    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET">
+
+                        <div>
+                            <p class="fw-semibold">Inserisci il numero di caratteri:</p>
+                            <input type="number" class="form-control bg-secondary text-white-50 shadow shadow-none" placeholder="Min 8 - Max 32" min="8" max="32" required name="pswnum">
+                        </div>
+
+                        <div class="py-3">
+                            <button type="submit" class="btn btn-outline-warning">GEN.</button>
+                        </div>
+
+                    </form>
+                </div>
+
+                <div>
+                    <div class="_mypassword">
+                        <p class="m-0"><?php echo $psw ?></p>
+                    </div>
+                </div>
+
+            </main>
+
+            <footer class="d-flex align-items-center p-4">
+                <h5 class="text-white-50 m-0 fs-6">Powered by PHP | Developed with &hearts; by Marco</h5>
+            </footer>
+        </div>
+        
     </div>
-</header>
 
-<main class="d-flex justify-content-center">
-    <div class="_mycontainer">
-
-        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET">
-
-            <div>
-                <p class="fw-semibold">Inserisci il numero di caratteri:</p>
-                <input type="number" class="form-control" placeholder="Min 8 - Max 32" min="8" max="32" required name="pswnum">
-            </div>
-
-            <div class="py-3">
-                <button type="submit" class="btn btn-outline-warning">Submit</button>
-            </div>
-
-        </form>
-    </div>
-</main>
-
-<footer>
-    
-</footer>
-    
 </body>
 </html>
